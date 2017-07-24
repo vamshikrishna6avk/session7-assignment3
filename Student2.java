90414286f27f2dea3e8214e2a529a66d7fd95b2a
vamshikrishna6avk <vamshikrishna6avk@gmail.com>
2017-07-24 15:14:32 +0530

    Add files via upload

---
+++ Student2.java
@@ -0,0 +1,16 @@
+package overloading;
+
+public class Student2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student s1 = new Student(1,"avk",1000);
+		Student s2 = new Student(2,"vamshi",10000);
+		Student s3 = new Student(3,"krishna",100);
+		s1.display();
+		s2.display();
+		s3.display();
+	}
+
+}
